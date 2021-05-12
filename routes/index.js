5c14b66ab1d89f4392fd9a52a1a6499095777e33
Christopher Bero <berocs@acedb.co>
2021-05-11 21:53:56 -0500

    Move index to my server.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const { exec } = require("child_process");
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 function gen_stl(req) {
