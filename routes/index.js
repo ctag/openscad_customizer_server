94a668a13c928293ea56b551831eaea7ff994793
Christopher Bero <berocs@acedb.co>
2021-05-11 21:45:53 -0500

    Switch to GET request for proxy pass.

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,17 @@ router.get('/', function(req, res, next) {
 });
 
 function gen_stl(req) {
+  //console.log(req.query);
   const timestamp = Date.now();
-  console.log("Time: ", timestamp);
-  const arg_part = ` -D part=\\"${req.body.part}\\"`
-  const arg_height = ` -D box_height=${req.body.height}`
-  const arg_width = ` -D box_width=${req.body.width}`
-  const arg_depth = ` -D box_depth=${req.body.depth}`
-  const arg_seal_h = ` -D seal_height=${req.body.seal_height}`
-  const arg_latch_num = ` -D latch_num=${req.body.latch_num}`
-  const arg_seal = ` -D seal_type=\\"${req.body.seal_type}\\"`
-  const output = `public/meshes/${timestamp}_${req.body.part}.stl`;
+  //console.log("Time: ", timestamp);
+  const arg_part = ` -D part=\\"${req.query.part}\\"`
+  const arg_height = ` -D box_height=${req.query.height}`
+  const arg_width = ` -D box_width=${req.query.width}`
+  const arg_depth = ` -D box_depth=${req.query.depth}`
+  const arg_seal_h = ` -D seal_height=${req.query.seal_height}`
+  const arg_latch_num = ` -D latch_num=${req.query.latch_num}`
+  const arg_seal = ` -D seal_type=\\"${req.query.seal_type}\\"`
+  const output = `public/meshes/${timestamp}_${req.query.part}.stl`;
   const baseCmd = `openscad -q -o ${output} penguin_case/penguin_case.scad`;
   return new Promise((resolve, reject) => {
     exec(baseCmd + arg_part + arg_height + arg_width + arg_depth + arg_seal_h + arg_latch_num + arg_seal,
@@ -36,7 +37,7 @@ function gen_stl(req) {
   })
 }
 
-router.post('/stl', async function(req, res, next) {
+router.get('/stl', async function(req, res, next) {
   const path = await gen_stl(req);
   res.download(path);
 });
